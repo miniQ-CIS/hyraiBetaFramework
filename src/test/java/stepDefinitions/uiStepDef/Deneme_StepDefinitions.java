cbd4e016bb741d5c7e2d7051259a88c196bcc1fa
Muzaffer Coban <muzzafferfatih@gmail.com>
2023-04-29 16:14:29 +0100

    Creating the deneme.feature and its stepDefinition + configuration.properties

---
+++ src/test/java/stepDefinitions/uiStepDef/Deneme_StepDefinitions.java
@@ -0,0 +1,11 @@
+package stepDefinitions.uiStepDef;
+
+import io.cucumber.java.en.Given;
+
+public class Deneme_StepDefinitions {
+
+    @Given("Step Definition icinde user and password")
+    public void step_definition_icinde_user_and_password() {
+        System.out.println("gasd");
+    }
+}
